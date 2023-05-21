04b4b12b27f4ec9ff8ce19e88f1efceb23817dec
pkryszk <pkryszk-github@yahoo.com>
2023-05-21 14:09:11 +0200

    styleing corrections

--- measurements/views.py
+++ measurements/views.py
@@ -1,18 +1,27 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from .models import Measurement
+from django.core.paginator import Paginator
 
+ITEMS_PER_PAGE = 12
 
 def measurement_list(request):
+
+
+    page_number = request.GET.get("page", 1)
+    objects_per_page = request.GET.get("per_page", ITEMS_PER_PAGE)
     measurements = Measurement.objects.all()
-    context = {
-        'measurements': measurements
-    }
-    return render(request, 'measurements/measurements_list.html', context)
+    p = Paginator(measurements, ITEMS_PER_PAGE)
+    page_obj = p.get_page(page_number)
+
+  #  context = {
+  #      'measurements': measurements
+  #  }
+    return render(request, 'measurements/measurements_list.html', {"page_obj": page_obj, "per_page": objects_per_page})
 
 
 def measurement_details(request, id):
     measurement = Measurement.objects.get(id=id)
-    return render(request, "measurements/measurement_details.html", {"measurement": measurement})
+    return render(request, "measurements/_measurement_details.html", {"measurement": measurement})
 
 
 def measurement_delete(request, id):
